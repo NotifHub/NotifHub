5a392a040ed14fc4eeff3a0ca1f0be3bee795299
Michelo <misamuna@gmail.com>
2016-06-29 00:11:12 +0100

    projet de base

---
+++ NotifHub/app/src/main/java/synapsehub/cd/notifhub/Config.java
@@ -0,0 +1,12 @@
+package synapsehub.cd.notifhub;
+
+/**
+ * Created by michelo on 6/28/16.
+ */
+
+public class Config {
+
+    public static final String BACKEND_URL = "https://syllasbryant.firebaseIO.com";
+
+
+}
